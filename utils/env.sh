a4f4288d93fd3eb7c839babdeb06950e6cd46c8c
Pablo E. Colazurdo <pabcol@amazon.com>
2021-01-28 16:47:40 +0000

    Added new nodegroups for upgrade to latest version and add support for SSM

--- utils/env.sh
+++ utils/env.sh
@@ -1,4 +1,6 @@
-CLUSTER_NAME=$(eksctl get cluster -o json | jq -r '.[].name')
+AWS_REGION=us-east-2
+CLUSTER_NAME=$(eksctl get cluster -o json | jq -r '.[].metadata.name')
 STACK_NAME=$(eksctl get nodegroup --cluster ${CLUSTER_NAME} -o json | jq -r '.[].StackName')
 ROLE_NAME=$(aws cloudformation describe-stack-resources --stack-name $STACK_NAME | jq -r '.StackResources[] | select(.ResourceType=="AWS::IAM::Role") | .PhysicalResourceId')
-echo "export ROLE_NAME=${ROLE_NAME}" 
+ROLE_NAME=${ROLE_NAME}
+alias k='kubectl'
